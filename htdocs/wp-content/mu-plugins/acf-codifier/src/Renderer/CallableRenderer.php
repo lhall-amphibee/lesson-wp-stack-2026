f0009698d3c67e53f006ed6c44362ad85ec6b85f
Loïc HALL <lhall@amphibee.fr>
2026-02-19 13:23:59 +0100

    Add condifier plugin

---
+++ htdocs/wp-content/mu-plugins/acf-codifier/src/Renderer/CallableRenderer.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * The callable renderer for Codifier components.
+ */
+
+namespace Geniem\ACF\Renderer;
+
+use Geniem\ACF\Interfaces\Renderer;
+
+/**
+ * Class Callable renderer
+ *
+ * Block renderer with simple methods.
+ *
+ * @package Geniem\ACF
+ */
+class CallableRenderer implements Renderer {
+
+    /**
+     * Method
+     *
+     * @var callable
+     */
+    protected $method;
+
+    /**
+     * Constructor
+     *
+     * @param callable $method The method to render the block with.
+     *
+     * @throws \Exception If template is not found.
+     */
+    public function __construct( callable $method ) {
+        $this->method = $method;
+    }
+
+    /**
+     * Renders ACF fields with the given method.
+     *
+     * @param array $fields ACF field values.
+     *
+     * @return string
+     */
+    public function render( array $fields ) : string {
+
+        // Filter the Renderer data.
+        $data = \apply_filters( 'codifier/callable-renderer/render/data', $fields[ 'data' ], $fields );
+
+        return \call_user_func( $this->method, $data );
+    }
+}
